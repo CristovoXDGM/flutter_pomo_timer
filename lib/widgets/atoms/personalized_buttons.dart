85851ebf9e96032d15ec97f3d9c1846cd40510b7
Cristovão Farias <cristovoxdgm@gmail.com>
2022-03-02 23:37:07 -0300

    Add new button icons and stylize clock

--- lib/widgets/atoms/personalized_buttons.dart
+++ lib/widgets/atoms/personalized_buttons.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class PersonalizedButton extends StatefulWidget {
-  const PersonalizedButton(
-      {Key? key,
-      required this.buttonText,
-      required this.onPress,
-      required this.buttonColor})
-      : super(key: key);
+  const PersonalizedButton({
+    Key? key,
+    required this.buttonIcon,
+    required this.onPress,
+    required this.buttonColorPressed,
+    required this.buttonColorHovered,
+    required this.buttonColorActive,
+  }) : super(key: key);
   final VoidCallback onPress;
-  final String buttonText;
-  final Color buttonColor;
+  final IconData buttonIcon;
+
+  final Color buttonColorPressed;
+  final Color buttonColorHovered;
+  final Color buttonColorActive;
+
   @override
   _PersonalizedButtonState createState() => _PersonalizedButtonState();
 }
@@ -19,16 +25,29 @@ class _PersonalizedButtonState extends State<PersonalizedButton> {
   Widget build(BuildContext context) {
     var currnetMediaQueryContext = MediaQuery.of(context);
 
-    return SizedBox(
+    return AnimatedContainer(
+      duration: const Duration(seconds: 1),
+      curve: Curves.bounceIn,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
       width: currnetMediaQueryContext.size.width / 3,
       child: ElevatedButton(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Text(widget.buttonText),
-        ),
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Icon(widget.buttonIcon)),
         onPressed: widget.onPress,
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(widget.buttonColor),
+          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+              (Set<MaterialState> states) {
+            if (states.contains(MaterialState.hovered)) {
+              return (widget.buttonColorHovered);
+            }
+
+            if (states.contains(MaterialState.focused) ||
+                states.contains(MaterialState.pressed)) {
+              return widget.buttonColorPressed;
+            }
+            return widget.buttonColorActive;
+          }),
         ),
       ),
     );
--- lib/widgets/pages/home_page.dart
+++ lib/widgets/pages/home_page.dart
@@ -11,13 +11,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool startClock = false;
-
   double secondsAmount = 25 * 60;
 
   late double currentMinutes = secondsAmount / 60;
   late double currentsec = secondsAmount % 60;
   late Timer _timer;
+
+  bool countdownStarted = false;
 
   updateTimer() {
     setState(() {
@@ -53,33 +53,51 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: currentTheme.colorScheme.primary,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: [
-          Center(
-            child: Text(
-              "${currentMinutes.floor().toString().padLeft(2, "0")}:${currentsec.floor().toString().padLeft(2, "0")}",
-              style: TextStyle(
-                fontSize: 30,
-                color: currentTheme.colorScheme.tertiary,
-              ),
-            ),
-          ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              PersonalizedButton(
-                buttonText: "start",
-                onPress: startCountdown,
-                buttonColor: currentTheme.colorScheme.secondary,
+              Center(
+                child: ClipOval(
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 200,
+                    height: 200,
+                    color: currentTheme.colorScheme.secondary,
+                    child: Text(
+                      "${currentMinutes.floor().toString().padLeft(2, "0")}:${currentsec.floor().toString().padLeft(2, "0")}",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: currentTheme.colorScheme.tertiary,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              PersonalizedButton(
-                buttonText: "Cancel",
-                onPress: () {
-                  resetTimer();
-                },
-                buttonColor: currentTheme.colorScheme.error,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  PersonalizedButton(
+                    buttonIcon: Icons.play_arrow,
+                    onPress: startCountdown,
+                    buttonColorPressed: currentTheme.colorScheme.tertiary,
+                    buttonColorHovered: currentTheme.colorScheme.primary,
+                    buttonColorActive: currentTheme.colorScheme.secondary,
+                  ),
+                  currentMinutes < 25
+                      ? PersonalizedButton(
+                          buttonIcon: Icons.stop,
+                          onPress: () {
+                            resetTimer();
+                          },
+                          buttonColorHovered: currentTheme.colorScheme.primary,
+                          buttonColorPressed: Colors.red.shade400,
+                          buttonColorActive: currentTheme.colorScheme.error,
+                        )
+                      : const SizedBox(),
+                ],
               ),
             ],
           ),
